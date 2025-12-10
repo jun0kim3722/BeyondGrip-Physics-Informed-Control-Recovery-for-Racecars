3d2cd9857fd76eff41fb699a1c279f68a385fe34
22averych <22averych@gmail.com>
2025-12-09 22:23:52 -0500

    updated to log phys reward

--- assetto_corsa_gym/AssettoCorsaEnv/recovery_phys_ac_env.py
+++ assetto_corsa_gym/AssettoCorsaEnv/recovery_phys_ac_env.py
@@ -1,9 +1,25 @@
 from AssettoCorsaEnv.recovery_env import RecoveryAssettoEnv
+from AssettoCorsaEnv.ac_env import logger
+
 import numpy as np
 
 class PhysicsRecoveryEnv(RecoveryAssettoEnv):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    
+    def get_reward(self, state, actions_diff, info):
+        r = super().get_reward(state, actions_diff, info)
+
+        # call original reward, but also call your debugging.
+        if (self.ep_steps % 50) == 0:
+                    logger.debug(f't: {self.ep_steps} speed: {state["speed"]:.2f}, oot: {state["out_of_track"]} '
+                                f's: {self.actions[0]:.2f} a: {self.actions[1]:.2f} b: {self.actions[2]:.2f} '
+                                f'reward: {state["reward"]:.3f} '
+                                f'done: {state["done"]:.0f} LapDist: {state["LapDist"]:.0f} gap: {state["gap"]:.1f} '
+                                )
+        return r
+
+
     
     def dense_reward(self, state, actions_diff, info):
         """
