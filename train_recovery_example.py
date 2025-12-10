d80f178132461791039847a255e764366e35acb0
22averych <22averych@gmail.com>
2025-12-09 21:14:04 -0500

    updated comments

--- train_recovery_example.py
+++ train_recovery_example.py
@@ -18,6 +18,12 @@ work_dir = 1
 # OR a slip recovery which is maintaining a slip angle of less than 7 degrees for 1 second.
 # This may be too long, but just put as an arbitrary value.
 
+# To use your own rewards, it is simple, you can inherrit RecoveryAssetoEnv again and then just override the two functions
+# for dense_reward and terminal_reward.
+
+# See assetto_corsa_gym/AssettoCorsaEnv/recovery_ac_env.py for comments on how that works and more specifics
+
+
 # The updates will not break any of the prewritten code (in theory).
 # If you do not env_class, it will default to using the AssettoEnv (non-recovery) and its associated rewards.
 
